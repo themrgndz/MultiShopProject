106fe011d16229e612070a8a5c8a96ec262a08bd
themrgndz <69049311+themrgndz@users.noreply.github.com>
2025-05-19 10:20:49 +0300

    19.05.2025

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductDetail
+    {
+        public string ProductDetailID { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductImage
+    {
+        public string ProductImageID { get; set; }
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }
+        public string Image3 { get; set; }
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
