14fc7c305eb8a51020e4c1e4a8d961f3acdd060d
themrgndz <69049311+themrgndz@users.noreply.github.com>
2025-05-20 23:22:52 +0300

    20.05.2025

--- Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/ResultProductDetailDto.cs
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/ResultProductDetailDto.cs
@@ -2,5 +2,10 @@
 {
     public class ResultProductDetailDto
     {
+        public string ProductDetailID { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+        public string ProductId { get; set; }
+
     }
 }
