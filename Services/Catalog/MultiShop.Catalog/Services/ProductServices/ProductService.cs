0323f9d820dd877437fc513a6207c99acb526408
themrgndz <69049311+themrgndz@users.noreply.github.com>
2025-05-22 00:42:44 +0300

    22.05.2025

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductServices/ProductService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.ProductServices
+{
+    public class ProductService : IProductService
+    {
+        private readonly IMapper _mapper;
+        private readonly IMongoCollection<Product> _productCollection;
+
+        public ProductService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _productCollection = database.GetCollection<Product>(_databaseSettings.ProductCollectionName);
+            _mapper = mapper;
+        }
+        public Task CreateProductAsync(CreateProductDto createProductDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdProductDto> GetByIdProductAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
