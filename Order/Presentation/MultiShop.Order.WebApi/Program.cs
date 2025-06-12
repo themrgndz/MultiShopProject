601631fcf6d5d7ab8e4f210b8bef6755f67ddab4
themrgndz <69049311+themrgndz@users.noreply.github.com>
2025-06-13 00:15:35 +0300

    13.06.2025

--- Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -16,7 +16,6 @@ builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
